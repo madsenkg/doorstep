10faf1978816c652747b958eb3337fd26d8b7ef2
madsen <102599096+madsenkg@users.noreply.github.com>
2023-08-30 15:22:55 +0200

    Update dingdong.ps1
    .

--- dingdong.ps1
+++ dingdong.ps1
@@ -13,11 +13,12 @@
   $GitRepro = [Microsoft.VisualBasic.Interaction]::InputBox("Github repro", "Enter the name of the private GitHub Repro")
 
 #See Source : https://stackoverflow.com/questions/63506725/using-powershell-to-download-file-from-private-github-repository-using-oauth
-  $credentials=$GitToken
+  $credentials="$GitToken"
   $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
   $headers.Add("Authorization", "token $credentials")
   $headers.Add("Accept", "application/json")
-  $download = ("https://raw.githubusercontent.com/{0}/main/{1}" -f $GitRepro, $file)
+  #$download = ("https://raw.githubusercontent.com/{0}/main/{1}" -f $GitRepro, $file)
+  $download = ("https://github.com/{0}/raw/main/{1}" -f $GitRepro, $file)
   Write-Host Headers $headers
   Write-Host Downloading $download 
   Invoke-WebRequest -Uri $download -Headers $headers -OutFile $file
