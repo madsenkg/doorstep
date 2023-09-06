7e5e86978e9b096f58e33a9556289562ee533b5a
Kenneth Madsen <kenneth.madsen@cgi.com>
2023-09-06 13:55:45 +0200

    Updated to handle Zipfile from Repo

--- dingdong.ps1
+++ dingdong.ps1
@@ -2,38 +2,45 @@
 #See Source : https://stackoverflow.com/questions/63506725/using-powershell-to-download-file-from-private-github-repository-using-oauth
 #Requires -RunAsAdministrator
     Clear-Host
-    Set-Location C:\temp
+    Set-Location $env:TEMP
     $ScriptFileName = ("{0}.ps1" -f [System.IO.Path]::GetFileNameWithoutExtension([System.IO.Path]::GetRandomFileName()))
-
+    $ZipFileName    = ("{0}.zip" -f [System.IO.Path]::GetFileNameWithoutExtension([System.IO.Path]::GetRandomFileName()))
+    $ZipFolder      = ("{0}"     -f [System.IO.Path]::GetFileNameWithoutExtension([System.IO.Path]::GetRandomFileName()))
 #Load Assembly
     [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
 
 #Dialog
     $d_credentials = [Microsoft.VisualBasic.Interaction]::InputBox("Github Token", "Enter your GitHub Token","<paste token here>")
-    $d_repo = [Microsoft.VisualBasic.Interaction]::InputBox("Github repro", "Enter the name of the private GitHub Repro <User/Repro>","madsenkg/Tokentest")
-    $d_file = [Microsoft.VisualBasic.Interaction]::InputBox("Run this file", "Enter the filename", "install.zip")
+    $d_repo        = [Microsoft.VisualBasic.Interaction]::InputBox("Github repo", "Enter the name of the private GitHub Repo <User/Repo>","<paste repo here>")
+    $d_file        = [Microsoft.VisualBasic.Interaction]::InputBox("Run this file", "Enter the filename", "Install.ps1")
  
     New-item -Name $ScriptFileName -ItemType File -Force | Out-Null
-    Add-Content -Path $ScriptFileName -Value 'Set-Location C:\temp'
-    Add-Content -Path $ScriptFileName -Value ('$credentials="{0}"' -f $d_credentials)
-    Add-Content -Path $ScriptFileName -Value ('$repo = "{0}"' -f $d_repo)
-    Add-Content -Path $ScriptFileName -Value ('$file = "{0}"' -f $d_file)
+    Add-Content -Path $ScriptFileName -Value 'Set-Location $env:TEMP'
     Add-Content -Path $ScriptFileName -Value '$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"'
-    Add-Content -Path $ScriptFileName -Value '$headers.Add("Authorization", "Bearer $credentials")'
-    Add-Content -Path $ScriptFileName -Value '$headers.Add("Accept", "*/*")'
-    #Add-Content -Path $ScriptFileName -Value '$download = "https://raw.githubusercontent.com/$repo/main/$file"'
-    Add-Content -Path $ScriptFileName -Value '$download = "https://github.com/$repo/archive/refs/heads/main.zip"'
-    
-    Add-Content -Path $ScriptFileName -Value 'Invoke-RestMethod -Uri $download -Headers $headers -Method Get -OutFile $file'
-<#  
+    Add-Content -Path $ScriptFileName -Value ('$headers.Add("Authorization", "Bearer {0}")' -f $d_credentials)
+    Add-Content -Path $ScriptFileName -Value '$headers.Add("Accept", "application/vnd.github+json")'
+    Add-Content -Path $ScriptFileName -Value ('$download = "https://api.github.com/repos/{0}/zipball"' -f $d_repo)
+    Add-Content -Path $ScriptFileName -Value ('Invoke-RestMethod -Uri $download -Headers $headers -Method Get -OutFile {0}' -f $ZipFileName)
+  
     if (Test-Path -Path .\$ScriptFileName -PathType Leaf) {
+        # Run Script file and remove it afterwards
         Start-Process "C:\Program Files\PowerShell\7\pwsh.exe" -Verb runAs -ArgumentList ".\$ScriptFileName" -Wait
-        
+        Remove-Item .\$ScriptFileName -Force
+
+        #Unzip repo file and remove it
+        Expand-Archive $ZipFileName -DestinationPath $ZipFolder
+        Remove-Item .\$ZipFileName -Force
+
+        #Find file in Zipfolder set location
+        Set-Location (Split-Path (Get-Childitem -Path $ZipFolder -Recurse |Where-Object {($_.name -eq $d_file)}| ForEach-Object{$_.FullName}))
+
+        # Run install file
         If (Test-Path -Path .\$d_file) {
             Start-Process "C:\Program Files\PowerShell\7\pwsh.exe" -Verb runAs -ArgumentList ".\$d_file" -WindowStyle Normal -Wait
             Remove-item .\$d_file -Force
         }
-        Remove-Item .\$ScriptFileName -Force
-       
-    }  
-#>    
+
+        # Cleaning up files
+        Set-Location $env:TEMP
+        Remove-item  -Path $ZipFolder -Recurse -Force 
+    }
