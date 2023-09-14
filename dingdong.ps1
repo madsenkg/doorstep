a96927a080418ddf40044f1e4fda6d55f71ec748
Kenneth Madsen <kenneth.madsen@cgi.com>
2023-09-14 12:33:33 +0200

    More Info

--- dingdong.ps1
+++ dingdong.ps1
@@ -8,6 +8,7 @@ Clear-Host
 $DotNetVersion = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where { $_.PSChildName -Match '^(?!S)\p{L}'} | Select PSChildName, version
 
 if ($DotNetVersion.version.Item(0) -gt 4.8) {
+    Write-output ".NET version is good !"    
 
     # Create Tmp folder for the script
     $TmpFolder      = ("C:\Temp\{0}.tmp"     -f [System.IO.Path]::GetFileNameWithoutExtension([System.IO.Path]::GetRandomFileName()))
